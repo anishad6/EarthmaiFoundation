fc92a77810c67cc832a4343eb50aa7ae6d42d99c
anishad6 <73513343+anishad6@users.noreply.github.com>
2026-01-06 12:22:44 +0530

    Update settings.py

--- Earthmai/settings.py
+++ Earthmai/settings.py
@@ -351,12 +351,21 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # =========================
 # EMAIL (OPTIONAL – Gmail)
 # =========================
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+# GMAIL SETTING 
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_PORT="587"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_HOST_USER="nishadabhishek200027@gmail.com"
+EMAIL_HOST_PASSWORD="zvfg dihc auuw tupi"
+EMAIL_USE_TLS=True
+
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 
 
